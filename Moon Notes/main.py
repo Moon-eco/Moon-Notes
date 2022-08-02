c8b5053e7ce1390128aa56fb45fa10cadbe0df00
!94#6666 <68169550+Najuky@users.noreply.github.com>
2022-08-02 20:05:53 +0200

    added version, functions, overall fix

--- Moon Notes/main.py
+++ Moon Notes/main.py
@@ -1,32 +1,64 @@
 import os
-from colorama import Fore, init
-
-init(autoreset=True)
+import ctypes
+from colorama import Fore
 
 ####Variables####
+
+version = str("0.0.1 Beta")
+
 color1 = Fore.MAGENTA
 color2 = Fore.MAGENTA
 fg = Fore.MAGENTA
 bg = Fore.MAGENTA
-name_template = "Moon Notes"
+project_name = "Moon Notes"
+
+ctypes.windll.kernel32.SetConsoleTitleW(f"{project_name} | {version}")
+
+def console_text(var_name, datatype, value):
+    var_name = datatype(input(f"\n{bg}[{fg}{project_name}{bg}]{fg} | {bg}value{fg}: "))
 
 def cls():
     os.system("cls")
 cls()
 
-def login():
+
+def setup():
     print("login")
 
+
 def create_notes():
-    print("create_notes")
+    print(console_text("note", str, "enter your note"))
+    # print(f"{note}")
+
+
+def note_list():
+    print("note list")
+
+
+def ascii_art():
+    moon_ascii = f"""
+                                        {fg}█{fg}█{fg}█{bg}╗   {fg}█{fg}█{fg}█{bg}╗ {fg}█{fg}█{fg}█{fg}█{fg}█{fg}█{bg}╗  {fg}█{fg}█{fg}█{fg}█{fg}█{fg}█{bg}╗ {fg}█{fg}█{fg}█{bg}╗   {fg}█{fg}█{bg}╗
+                                        {fg}█{fg}█{fg}█{fg}█{bg}╗ {fg}█{fg}█{fg}█{fg}█{bg}║{fg}█{fg}█{bg}╔═══{fg}█{fg}█{bg}╗{fg}█{fg}█{bg}╔═══{fg}█{fg}█{bg}╗{fg}█{fg}█{fg}█{fg}█{bg}╗  {fg}█{fg}█{bg}║
+                                        {fg}█{fg}█{bg}╔{fg}█{fg}█{fg}█{fg}█{bg}╔{fg}█{fg}█{bg}║{fg}█{fg}█{bg}║   {fg}█{fg}█{bg}║{fg}█{fg}█{bg}║   {fg}█{fg}█{bg}║{fg}█{fg}█{bg}╔{fg}█{fg}█{bg}╗ {fg}█{fg}█{bg}║
+                                        {fg}█{fg}█{bg}║{bg}╚{fg}█{fg}█{bg}╔{bg}╝{fg}█{fg}█{bg}║{fg}█{fg}█{bg}║   {fg}█{fg}█{bg}║{fg}█{fg}█{bg}║   {fg}█{fg}█{bg}║{fg}█{fg}█{bg}║{bg}╚{fg}█{fg}█{bg}╗{fg}█{fg}█{bg}║
+                                        {fg}█{fg}█{bg}║ {bg}╚═{bg}╝ {fg}█{fg}█{bg}║{bg}╚{fg}█{fg}█{fg}█{fg}█{fg}█{fg}█{bg}╔{bg}╝{bg}╚{fg}█{fg}█{fg}█{fg}█{fg}█{fg}█{bg}╔{bg}╝{fg}█{fg}█{bg}║ {bg}╚{fg}█{fg}█{fg}█{fg}█{bg}║
+                                        {bg}╚═{bg}╝     {bg}╚═{bg}╝ {bg}╚═════{bg}╝  {bg}╚═════{bg}╝ {bg}╚═{bg}╝  {bg}╚═══{bg}╝
+"""
+    print(moon_ascii)
+
 
 def menu():
-    print(f"                    {color1}[{fg}1{color1}]  {fg}Create Note                                         Notes List {color2}[{fg}2{color2}]")
-    option_choice = input(f"\n{bg}[{fg}{name_template}{bg}]{fg} | {bg}Choice{fg}: ")
+    print(f"                    {color1}[{fg}1{color1}] {fg}Create Note                                         Notes List {color2}[{fg}2{color2}]")
+    option_choice = input(f"\n{bg}[{fg}{project_name}{bg}]{fg} | {bg}Choice{fg}: ")
     cls()
-    menu()
     if option_choice == "1":
         create_notes()
+    elif option_choice == "2":
+        note_list()
 
-login()
+
+# login()
+ascii_art()
 menu()
+
+# # vars and console title (!IMPORTANT)
